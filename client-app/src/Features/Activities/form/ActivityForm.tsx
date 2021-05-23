0f20bef8d792ee7b33e93f635f60f652486f88c9
ifratmitul <ifrat.mitul@hotmail.com>
2021-05-23 18:03:06 +0600

    added reusable input elements

--- client-app/src/Features/Activities/form/ActivityForm.tsx
+++ client-app/src/Features/Activities/form/ActivityForm.tsx
@@ -1,11 +1,17 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
-import { Button, Form, Segment } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Button, Segment } from "semantic-ui-react";
 import { useStore } from "../../../App/stores/store";
 import { observer } from "mobx-react-lite";
 import { useHistory, useParams } from "react-router";
 import LoadingComponent from "../../../App/Layout/LoadingComponent";
 import { Link } from "react-router-dom";
-import { v4 as uuid } from "uuid";
+import { Formik, Form } from "formik";
+import * as yup from "yup";
+import MyTextInput from "../../../App/common/form/MyTextInput";
+import MyTextArea from "../../../App/common/form/MyTextArea";
+import MySelectInput from "../../../App/common/form/MySelectInput";
+import { categoryOptions } from "../../../App/common/options/categoryOptions";
+import MyDateInput from "../../../App/common/form/MyDateInput";
 
 export default observer(function ActivityForm() {
   const history = useHistory();
@@ -24,90 +30,86 @@ export default observer(function ActivityForm() {
     venue: "",
   });
 
+  const validationSchema = yup.object({
+    title: yup.string().required("There must be a title"),
+    description: yup.string().required("There must be a description"),
+    city: yup.string().required("There must be a city"),
+    venue: yup.string().required("There must be a venue"),
+    category: yup.string().required("There must be a category"),
+  });
+
   useEffect(() => {
     if (id) loadActivity(id).then((activity) => setActivity(activity!));
   }, [id, loadActivity]);
 
-  function handleSubmit() {
-    //console.log(activity);
-    if (activity.id.length === 0) {
-      let newActivity = {
-        ...activity,
-        id: uuid(),
-      };
-      createActivity(newActivity).then(() =>
-        history.push(`/activities/${newActivity.id}`)
-      );
-    } else {
-      updateActivity(activity).then(() =>
-        history.push(`/activities/${activity.id}`)
-      );
-    }
-  }
+  // function handleSubmit() {
+  //   //console.log(activity);
+  //   if (activity.id.length === 0) {
+  //     let newActivity = {
+  //       ...activity,
+  //       id: uuid(),
+  //     };
+  //     createActivity(newActivity).then(() =>
+  //       history.push(`/activities/${newActivity.id}`)
+  //     );
+  //   } else {
+  //     updateActivity(activity).then(() =>
+  //       history.push(`/activities/${activity.id}`)
+  //     );
+  //   }
+  // }
 
-  function handleInputChang(
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const { name, value } = event.target;
-    setActivity({ ...activity, [name]: value });
-  }
+  // function handleInputChang(
+  //   event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  // ) {
+  //   const { name, value } = event.target;
+  //   setActivity({ ...activity, [name]: value });
+  // }
   if (loadingInitial)
     return <LoadingComponent content="Loading Activity ....." />;
   return (
     <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete="off">
-        <Form.Input
-          placeholder="Title"
-          value={activity.title}
-          name="title"
-          onChange={handleInputChang}
-        />
-        <Form.TextArea
-          placeholder="Description"
-          value={activity.description}
-          name="description"
-          onChange={handleInputChang}
-        />
-        <Form.Input
-          placeholder="Category"
-          value={activity.category}
-          name="category"
-          onChange={handleInputChang}
-        />
-        <Form.Input
-          placeholder="Date"
-          type="date"
-          value={activity.date}
-          name="date"
-          onChange={handleInputChang}
-        />
-        <Form.Input
-          placeholder="City"
-          value={activity.city}
-          name="city"
-          onChange={handleInputChang}
-        />
-        <Form.Input
-          placeholder="Venue"
-          value={activity.venue}
-          name="venue"
-          onChange={handleInputChang}
-        />
-        <Button
-          // loading={loading}
-          floated="right"
-          positive
-          type="submit"
-          content="Submit"
-        />
-        <Button
-          as={Link}
-          to="/activities"
-          floated="right"
-          type="button"
-          content="Cancel"
-        />
-      </Form>
+      <Formik
+        validationSchema={validationSchema}
+        enableReinitialize
+        initialValues={activity}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values: activity, handleChange, handleSubmit }) => (
+          <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+            <MyTextInput name="title" placeholder="Title" />
+            <MyTextArea rows={3} name="description" placeholder="Description" />
+            <MySelectInput
+              options={categoryOptions}
+              name="category"
+              placeholder="Category"
+            />
+            <MyDateInput
+              placeholderText="Date"
+              name="date"
+              showTimeSelect
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm aa"
+            />
+            <MyTextInput name="city" placeholder="City" />
+            <MyTextInput name="venue" placeholder="Venue" />
+            <Button
+              // loading={loading}
+              floated="right"
+              positive
+              type="submit"
+              content="Submit"
+            />
+            <Button
+              as={Link}
+              to="/activities"
+              floated="right"
+              type="button"
+              content="Cancel"
+            />
+          </Form>
+        )}
+      </Formik>
     </Segment>
   );
 });
