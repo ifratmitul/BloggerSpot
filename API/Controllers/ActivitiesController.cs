cbb6d389afc37f177141f37d1a21ed6e171ef39e
ifratmitul <ifrat.mitul@hotmail.com>
2021-07-21 18:24:50 +0600

    Identity implementation complete

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -18,7 +19,6 @@ namespace API.Controllers
             // return await _context.Activities.ToListAsync();
             return HandleResult(await Mediator.Send(new List.Query()));
         }
-
 
         [HttpGet("{id}")]
 
